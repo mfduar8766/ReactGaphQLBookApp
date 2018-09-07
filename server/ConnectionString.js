1ab879e9052a996b9a7f8835638185e2f55f01d0
Matheus Duarte <mfduar8766@gmail.com>
2018-09-07 09:40:34 -0400

    Added connection string and gitignore.

---
+++ server/ConnectionString.js
@@ -0,0 +1,2 @@
+const URL = "mongodb://user1:Schecterguitars0060792!@ds239988.mlab.com:39988/gql-react";
+module.exports = URL;
--- server/app.js
+++ server/app.js
@@ -1,21 +1,28 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
-const schema = require('./schema/schema');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const url = require("./ConnectionString");
 
 const app = express();
 app.use(cors());
 
-mongoose.connection.once('open', () => {
-    console.log('connected to DB.');
+mongoose.connect(url);
+
+mongoose.connection.once("open", () => {
+  console.log("connected to DB.");
 });
 
-app.use('/graphql', graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema,
     graphiql: true
-}));
+  })
+);
 
-app.listen(4000, () => {
-    console.log('Running on port: 4000');
+app.listen(port, () => {
+  console.log("Running on port: 4000");
 });
