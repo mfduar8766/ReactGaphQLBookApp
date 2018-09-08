5931aad08c0785c390a9b24529ca50abe9a9eb1d
Matheus Duarte <mfduar8766@gmail.com>
2018-09-08 17:24:26 -0400

    Added a config folder to hold key.

--- server/ConnectionString.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  mongoURI:"mongodb://user:Schecterguitars0060792!@ds239988.mlab.com:39988/gql-react"
-};
--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const schema = require("./schema/schema");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const url = require("./ConnectionString");
+const url = require("./Config/ConnectionString");
 
 const app = express();
 app.use(cors());
